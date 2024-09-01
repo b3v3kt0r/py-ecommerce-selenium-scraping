eb7a969c16b109924424fb477cf9f775bdb1a2b1
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-01 21:12:44 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import (
     NoSuchElementException,
-    ElementClickInterceptedException
+    ElementClickInterceptedException,
 )
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -19,8 +19,14 @@ TABLETS_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/computers/tablets")
 PHONES_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/phones")
 TOUCH_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/phones/touch")
 
-URLS_FIELDS = [HOME_URL, COMPUTERS_URL, LAPTOPS_URL,
-               TABLETS_URL, PHONES_URL, TOUCH_URL]
+URLS_FIELDS = [
+    HOME_URL,
+    COMPUTERS_URL,
+    LAPTOPS_URL,
+    TABLETS_URL,
+    PHONES_URL,
+    TOUCH_URL
+]
 
 
 @dataclass
@@ -56,13 +62,14 @@ def parse_single_product(product_soup: BeautifulSoup) -> Product:
     )
 
 
-def get_all_products(url: str, driver: WebDriver) -> [Product]:
+def get_all_products(url: str, driver: WebDriver) -> list[Product]:
     driver.get(url)
 
     while True:
         try:
-            more_button = (driver.find_element
-                           (By.CLASS_NAME, "ecomerce-items-scroll-more"))
+            more_button = driver.find_element(
+                By.CLASS_NAME, "ecomerce-items-scroll-more"
+            )
             more_button.click()
         except (NoSuchElementException, ElementClickInterceptedException):
             break
@@ -81,7 +88,7 @@ def write_quotes_to_csv(products: [Product], filename: str) -> None:
         writer.writerows([astuple(product) for product in products])
 
 
-def parse_urls(driver: WebDriver, urls: list) -> [Product]:
+def parse_urls(driver: WebDriver, urls: list) -> list[Product]:
     all_products = []
     for url in urls:
         all_products.extend(get_all_products(url, driver))
